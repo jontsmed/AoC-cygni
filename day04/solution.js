2f1bc16ba008e1f11ab1d84624683beae1b09dd0
Jonathan Smedborn <jonathan.smedborn@gmail.com>
2022-12-04 21:32:39 +0100

    day 4

---
+++ day04/solution.js
@@ -0,0 +1,30 @@
+import { file } from "bun";
+const input = (await file("input.txt").text()).trim().split("\n");
+
+const getPartOneSolution = () => {
+  console.log(
+    input.reduce((overlaps, pair) => {
+      const [[elf1Start, elf1End], [elf2Start, elf2End]] = pair
+        .split(",")
+        .map((range) => range.split("-").map((id) => +id));
+
+      const diffStart = elf1Start - elf2Start;
+      const diffEnd = elf1End - elf2End;
+      if ((diffStart < 0 && diffEnd < 0) || (diffStart > 0 && diffEnd > 0))
+        return overlaps;
+      return overlaps + 1;
+    }, 0)
+  );
+};
+const getPartTwoSolution = () => {
+  console.log(
+    input.reduce((overlaps, pair) => {
+      const [[elf1Start, elf1End], [elf2Start, elf2End]] = pair
+        .split(",")
+        .map((range) => range.split("-").map((id) => +id));
+      if (elf1End < elf2Start || elf2End < elf1Start) return overlaps;
+      return overlaps + 1;
+    }, 0)
+  );
+};
+process.env.part == "part1" ? getPartOneSolution() : getPartTwoSolution();
